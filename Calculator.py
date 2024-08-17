2794164263c454752e7d97a32fbf514ccda99984
KubaBorow <jakubrcam@gmail.com>
2024-08-17 20:11:44 +0200

    Adding some changes in main file. Add About page

--- Calculator.py
+++ Calculator.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import functions
+import time
 
+
+st.set_page_config(
+    page_title="Calculator"
+)
 # theme[]
 
 todos = functions.get_todos()
-
-
-# number = functions.caculator(weight='kg', height='cm')
 
 
 def add_todo():
@@ -16,8 +18,6 @@ def add_todo():
     st.session_state["new_todo"] = ""
 
 
-st.title("My BMI Calculator")
-st.subheader("This is my BMI calculator.")
 st.write('This app is to increase Your health.')
 
 col1, col2 = st.columns(2)
@@ -37,11 +37,13 @@ try:
     else:
         st.write('You need go to see a doctor.')
 
-
 except TypeError:
     st.write('Try another number')
 
-    st.subheader("Add your meals under: ")
+col3, col4 = st.columns(2)
+col3.text_input("Enter today`s date: ")
+col4.text_input("How do you rate your mood today from 1 to 10? ")
+st.subheader("Add your meals under: ")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -53,3 +55,12 @@ for index, todo in enumerate(todos):
 
 st.text_input(label=' ', placeholder='Add meals...',
               on_change=add_todo, key='new_todo')
+
+# save_btn = st.button('Save', type='secondary')
+
+# if save_btn:
+#     with st.spinner('Saving...'):
+#         time.sleep(3)
+    # with open(f'../journal/{date}.txt', 'w') as file:
+    # file.write(mood + 2 * '\n')
+    # file.write(thoughts)
---
+++ pages/1_About.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('Hello !! 👋')
+st.subheader('This is my first BMI calculator.')
+st.write('You have to enter your height and weight, after that you get your result. Also you can add your meals '
+         'that u ate all day.')
